7dbb45f62d2bcca86af9714a7c62197a4f9eb2bf
Arai Shinya <>
2019-04-03 17:52:21 +0900

    難しくなってきた、HTTPわからん

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -12,13 +12,18 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private heroService: HeroService,
   ) { }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 
   ngOnInit(): void {
     this.getHero();
